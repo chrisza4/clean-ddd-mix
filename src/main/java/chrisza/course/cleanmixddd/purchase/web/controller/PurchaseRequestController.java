2cd0b82a815a807bc6541ecc6a52394484e69b3a
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2022-02-09 21:33:01 +0700

    Implement controller

--- src/main/java/chrisza/course/cleanmixddd/purchase/web/controller/PurchaseRequestController.java
+++ src/main/java/chrisza/course/cleanmixddd/purchase/web/controller/PurchaseRequestController.java
@@ -1,18 +1,24 @@
 package chrisza.course.cleanmixddd.purchase.web.controller;
 
+import chrisza.course.cleanmixddd.purchase.DependencyResolver;
+import chrisza.course.cleanmixddd.purchase.domain.entities.PurchaseRequest;
+import chrisza.course.cleanmixddd.purchase.domain.exceptions.UnapprovableException;
 import chrisza.course.cleanmixddd.purchase.domain.valueobjects.NewPurchaseRequest;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PurchaseRequestController {
 
     @PostMapping("/pr")
-    public NewPurchaseRequest create(@RequestBody NewPurchaseRequest model) {
-        return model;
+    public PurchaseRequest create(@RequestBody NewPurchaseRequest model) {
+        var service = DependencyResolver.getService();
+        return service.AddNewPurchaseRequest(model);
     }
 
     @PostMapping("/pr")
-    public NewPurchaseRequest approve(@RequestBody NewPurchaseRequest model) {
-        return model;
+    public PurchaseRequest approve(int id) throws UnapprovableException, ChangeSetPersister.NotFoundException {
+        var service = DependencyResolver.getService();
+        return service.Approve(id);
     }
 }
